f12c5744fee12ef390825376a4bec5cc140d516c
Byers <M2003744@middlesbro.ac.uk>
2022-09-28 12:46:41 +0100

    Odd or Even number

---
+++ IsTheNumberOddOrEven.php
@@ -0,0 +1,12 @@
+<?php
+// Enter your code here, enjoy!
+$number=42; // Declare number variable
+if($number %2==0) // If the number is divisable by 2
+{
+	echo "$number is an even number.";
+}
+else 
+{
+	echo "$number is an odd number.";
+}
+?>
