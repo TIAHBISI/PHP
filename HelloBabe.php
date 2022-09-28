1e2ed764835958205447b3edca6853718b3cd443
Byers <M2003744@middlesbro.ac.uk>
2022-09-28 12:33:10 +0100

    first commit

---
+++ HelloBabe.php
@@ -0,0 +1,6 @@
+<?php
+// Enter your code here, enjoy!
+$foo = array("Hello", "Goodbye", "Babe"); // Array of integers created using array function
+echo $foo[0]; //returns Hello
+echo $foo[2]; //returns Babe
+?>
