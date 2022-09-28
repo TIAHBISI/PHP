50353617bf4dda38ea29aac7095ec3de62416120
Byers <M2003744@middlesbro.ac.uk>
2022-09-28 13:31:12 +0100

    Multiply a number by itself

---
+++ MuiltiplyBySelf.php
@@ -0,0 +1,10 @@
+<?php
+$number = 5; // The number that is being multiplied
+$loop = 0; // Loop
+$num1 = 1; 
+while($num1 <= 100) {
+	$loop++;
+	echo "Loop ",$loop;
+	echo " Answer = ", $num1 = $num1 * $number,"\n ";
+}
+?>
